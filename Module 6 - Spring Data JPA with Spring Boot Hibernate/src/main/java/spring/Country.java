fd1433c05723e35b1c8319bea42b2dbc73ddf7ae
Rithikaa-c <230200.cd@rmkec.ac.in>
2026-07-02 14:35:05 +0530

    module 6

---
+++ Module 6 - Spring Data JPA with Spring Boot Hibernate/src/main/java/spring/Country.java
@@ -0,0 +1,36 @@
+package com.cognizant.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Country {
+
+    @Id
+    private String code;
+    private String name;
+
+    public Country() {
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return code + " " + name;
+    }
+}
